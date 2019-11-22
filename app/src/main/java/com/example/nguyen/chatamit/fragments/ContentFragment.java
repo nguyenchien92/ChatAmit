bd03a48cd4378b09c5b4ff3babbbb2b419e8f3fa
Nguyen Chien <chiennxd00245@fpt.edu.vn>
2019-11-22 17:21:01 +0700

    completed navigate in ChatFragScreen

--- app/src/main/java/com/example/nguyen/chatamit/fragments/ContentFragment.java
+++ app/src/main/java/com/example/nguyen/chatamit/fragments/ContentFragment.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.nguyen.chatamit.R;
@@ -101,18 +103,32 @@ public class ContentFragment extends BaseFragment {
 
         int count = childMn.getBackStackEntryCount();
 
-        if(count > 0)
-        {
-            childMn.popBackStack();
-            bottom_navigation_bar.setVisibility(View.VISIBLE);
+        if (count > 0) {
 
-            return true;
-        }else {
-            if(frag instanceof MessContainerFragment)
-            {
+            Fragment childFrag = childMn.getFragments().get(0);
+
+            if (childFrag instanceof ChatScreenFrag) {
+                View view = childFrag.getView();
+
+                RecyclerView rvStick = view.findViewById(R.id.rv_sticker_screen);
+
+
+                if(rvStick.getVisibility() == View.VISIBLE)
+                {
+                    rvStick.setVisibility(View.GONE);
+                    return true;
+                }
+
+            }
+                childMn.popBackStack();
+                bottom_navigation_bar.setVisibility(View.VISIBLE);
+
+                return true;
+        } else {
+            if (frag instanceof MessContainerFragment) {
                 showPopUp();
                 return true;
-            }else {
+            } else {
                 bottom_navigation_bar.setSelectedItemId(R.id.action_message);
 
                 return true;
@@ -145,3 +161,36 @@ public class ContentFragment extends BaseFragment {
         });
     }
 }
+//
+//        if (count > 0) {
+//
+//                if (current instanceof ChatScreenFrag) {
+//                View view = current.getView().findViewById(R.id.include_bottom);
+//                RecyclerView rvStickerView = view.findViewById(R.id.rv_sticker_screen);
+//
+//                if (rvStickerView.getVisibility() == View.VISIBLE) {
+//                rvStickerView.setVisibility(View.GONE);
+//                childFm.popBackStack(current.getTag(), 0);
+//                } else {
+//                childBackFragment(childFm, bottom);
+//                }
+//                } else {
+//
+//                childBackFragment(childFm, bottom);
+//                }
+//
+//                } else {
+//                if (!(current instanceof MessageFragment)) {
+//                bottom.setSelectedItemId(R.id.action_message);
+//                } else {
+//                showPopUp();
+//                }
+//                }
+//                }
+//
+//
+////This method is temporary... no idea instead :((
+//private void childBackFragment(FragmentManager fm, BottomNavigationView bottomNavigationView) {
+//        fm.popBackStack(fm.getFragments().get(0).getTag(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
+//        bottomNavigationView.setVisibility(View.VISIBLE);
+//        }
